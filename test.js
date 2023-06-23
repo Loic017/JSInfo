7949ef594321fb0f1baeefea29083e73eecce722
Loic <l.loic017@gmail.com>
2023-06-23 20:45:06 +0200

    p

---
+++ test.js
@@ -0,0 +1,11 @@
+
+/* 
+Create a web-page that asks for a name and outputs it.
+https://javascript.info/alert-prompt-confirm
+*/
+
+// let nameInput = prompt("What is your name?");
+// if (nameInput.length == 0) {
+//     nameInput = "Anonymous"
+// }
+// alert("Your name is " + nameInput)
